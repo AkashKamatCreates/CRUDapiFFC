6b8e4ebc235f6d724b36ac5a1a9d9b8403ac520b
AkashKamatCreates <kamatakash13@gmail.com>
2024-02-25 13:35:46 +0530

    model

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose')
+const Product = require('./models/product.model');
 const app = express();
+app.use(express.json());
+
+
+app.post('/api/products', async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500),json({message:error.message});
+    }
+});
+
+
+
+app.post('/api/products', async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500),json({message:error.message});
+    }
+});
+
+
+
 
 
 app.get('/', (req, res)=>{
