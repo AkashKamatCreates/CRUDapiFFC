30aef9c4404e9352cfb42c99f64eb8371ca64fed
AkashKamatCreates <kamatakash13@gmail.com>
2024-02-25 10:53:35 +0530

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const express = require('express');
-const app = express()
+const app = express();
+
+app.listen(3000, ()=>{
+    console.log("hello akash")
+});
+
+app.get('/', (req, res)=>{
+    res.send('hello from nodeapi')
+});
