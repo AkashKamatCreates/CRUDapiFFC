5745a72cd5bb3dff50e77f453223536296e78026
AkashKamatCreates <kamatakash13@gmail.com>
2024-02-25 12:31:02 +0530

    models

--- models/product.model.js
+++ models/product.model.js
@@ -1,27 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const ProductSchema = mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: [true, "Please enter product name"],
-        },
-        quantity: {
-            type: Number, 
-            required: [true, "Please enter quantity of the product"],
-            default: 0,
-        },
-        price: {
-            type: Number,
-            require: true,
-            default: 0,
-        },
-        image: {
-            type: String,
-            required: false,
-        }
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter product name"],
     },
-    {
-        timestamps: true,
-    }
+    quantity: {
+      type: Number,
+      required: [true, "Please enter quantity of the product"],
+      default: 0,
+    },
+    price: {
+      type: Number,
+      require: true,
+      default: 0,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
+
+const Product = mongoose.model("Product", ProductSchema);
+module.exports = Product;
