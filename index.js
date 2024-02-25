ce889ffe874f53d9b56cf56413ad897ec43752cc
AkashKamatCreates <kamatakash13@gmail.com>
2024-02-25 12:16:44 +0530

    mongodb init and integrated

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose')
 const app = express();
 
-app.listen(3000, ()=>{
-    console.log("Server Started on localhost:3000")
-});
 
 app.get('/', (req, res)=>{
-    res.send('hello from nodeapi server testing ')
+    res.send('hello from nodeapi')
 });
 
 
 mongoose.connect("mongodb+srv://yourcare316:OK05rQM1HGOQqhfU@backenddatabase.thivddw.mongodb.net/NodeAPI?retryWrites=true&w=majority&appName=BackendDatabase")
 .then(() => {
     console.log('Connected to the database')
+    app.listen(3000, ()=>{
+        console.log("Server Started on localhost:3000")
+    });
 })
 .catch(()=>{
     console.log('NOT CONNECTED!!!!')
