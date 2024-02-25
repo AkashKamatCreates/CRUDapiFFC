9c640b0058709547d3d20d54bdca6c5145f09102
AkashKamatCreates <kamatakash13@gmail.com>
2024-02-25 10:54:26 +0530

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.listen(3000, ()=>{
-    console.log("hello akash")
+    console.log("Server Started on localhost:3000")
 });
 
 app.get('/', (req, res)=>{
